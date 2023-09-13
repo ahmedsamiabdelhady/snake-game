de49086a3cccc8335e251b09283a8713f23503f6
Ahmed Sami <144848952+ahmedsamiabdelhady@users.noreply.github.com>
2023-09-13 16:19:56 +0300

    Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -4,51 +4,50 @@ from food import Food
 from scoreboard import ScoreBoard
 import time
 
-
 screen = Screen()
-screen.setup(600, 600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
-
 
 snake = Snake()
 food = Food()
-score = ScoreBoard()
-score.score()
-
-
-
-
+scoreboard = ScoreBoard()
+scoreboard.score()
 
 screen.listen()
-screen.onkey(snake.up, "w")
-screen.onkey(snake.down, "s")
-screen.onkey(snake.right, "d")
-screen.onkey(snake.left, "a")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    if snake.head.distance(food) < 20:
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score.increase_score()
+        scoreboard.increase_score()
 
-    elif snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 300 or snake.head.ycor() < -295:
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
-        score.gameover()
+        scoreboard.gameover()
 
-
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
             game_is_on = False
-            score.gameover()
+            scoreboard.gameover()
 
 
 
 
 
-    screen.exitonclick()
-
+screen.exitonclick()
